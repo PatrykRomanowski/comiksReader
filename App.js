c7a6978bdac67e07b40955ceaeb88d3302ab26e9
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-03-06 13:40:23 +0100

    adding icons to navigation

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 // import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Button, Text, View, FlatList } from "react-native";
-// import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack
+import { Ionicons } from "@expo/vector-icons";
 
 import { NavigationContainer } from "@react-navigation/native";
 import IndexScreen from "./src/screens/indexScreen";
 import TestScreen from "./src/screens/testScreen";
+import LoginScreen from "./src/screens/loginScreen";
 
 // const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -23,15 +25,49 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Index1" component={IndexScreen}></Tab.Screen>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName; // selection of icons
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "library") {
+              iconName = "library";
+            } else if (route.name === "book") {
+              iconName = "book";
+            } else if (route.name === "login") {
+              iconName = "person-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+          style: { height: 60 },
+          labelStyle: { fontSize: 12 },
+        }}
+      >
         <Tab.Screen
-          name="Test"
-          component={TestScreen}
+          name="home"
+          component={IndexScreen}
           options={{
-            headerTitle: "Test2",
-            headerRight: () => <Button title="Test2"></Button>,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
           }}
+        ></Tab.Screen>
+        <Tab.Screen name="book" component={IndexScreen}></Tab.Screen>
+        <Tab.Screen name="library" component={LoginScreen}></Tab.Screen>
+        <Tab.Screen
+          name="login"
+          component={TestScreen}
+          // options={{
+          //   headerTitle: "Test2",
+          //   headerRight: () => <Button title="Test2"></Button>,
+          // }}
         ></Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
---
+++ src/screens/loginScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, TextInput, Text, View, StyleSheet } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>This is Login Screen!!!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default LoginScreen;
